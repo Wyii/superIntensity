d7408328baae5b4e54d6ec3ae3c25e16915864a5
Wen Yi <406905900@qq.com>
2018-10-13 22:53:50 +0800

    更新版本

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+const Index = () => {
+    return <div>Hello React!</div>
+}
+
+ReactDOM.render(<Index />, document.getElementById('index'));
--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,17 @@
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+
 module.exports = {
-    entry: __dirname + "/app/main.js",
-    devtool: 'eval-source-map',
-    output: {
-        path: __dirname + "/public",
-        filename: "bundle.js"
-    },
-    devServer: {
-        contentBase: "./public",
-        historyApiFallback: true,
-        inline: true
-    },
+    // entry: __dirname + "/app/main.js",
+    // devtool: 'eval-source-map',
+    // output: {
+    //     path: __dirname + "/public",
+    //     filename: "bundle.js"
+    // },
+    // devServer: {
+    //     contentBase: "./public",
+    //     historyApiFallback: true,
+    //     inline: true
+    // },
     module: {
         rules: [
             {
@@ -18,7 +20,18 @@ module.exports = {
                     loader: "babel-loader"
                 },
                 exclude: /node_modules/
-            }
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            },
+            
         ]
-    }
-}
+    },
+    plugins: [
+        new HtmlWebPackPlugin({
+            template: './src/index.html',
+            filename: './index.html'
+        })
+    ]
+};
