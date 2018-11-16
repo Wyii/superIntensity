009da59b574053840c15095daca250ede9921525
Wen Yi <406905900@qq.com>
2018-11-16 22:19:47 +0800

    init

--- webpack.config.js
+++ webpack.config.js
@@ -1,17 +1,6 @@
 const HtmlWebPackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    // entry: __dirname + "/app/main.js",
-    // devtool: 'eval-source-map',
-    // output: {
-    //     path: __dirname + "/public",
-    //     filename: "bundle.js"
-    // },
-    // devServer: {
-    //     contentBase: "./public",
-    //     historyApiFallback: true,
-    //     inline: true
-    // },
     module: {
         rules: [
             {
